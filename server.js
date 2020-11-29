a3c55c4a39cc7a0bd3bba2c963a8e3a81b096b9d
Nick Bohannan <nbohannan@gmail.com>
2020-11-29 17:57:34 -0600

    port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app)
 
 app.use(express.static(__dirname + '/public'))
 
-const port = process.env.PORT || 8083
+const port = 8083
  
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
