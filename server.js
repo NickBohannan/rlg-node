23316633071ff81470b6b7e320dd75d233f875ee
Nick Bohannan <nbohannan@gmail.com>
2020-11-29 17:18:59 -0600

    port change

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express()
 const server = require('http').Server(app)
 
 app.use(express.static(__dirname + '/public'))
+
+const port = process.env.PORT || 8083
  
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
-server.listen(8083, function () {
+server.listen(port, function () {
     console.log(`Listening on ${server.address().port}`)
 })
